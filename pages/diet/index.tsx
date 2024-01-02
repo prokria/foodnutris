96a87f3588b3feb7df55cc39e8d51540535f8548
Jahangir Alom <jahangir.alom@portonics.com>
2024-01-02 22:19:53 +0600

    Food Chart table title

--- pages/diet/index.tsx
+++ pages/diet/index.tsx
@@ -29,7 +29,7 @@ const DietChart: NextPage = () => {
           <Table sx={{ minWidth: 650 }} aria-label="simple table">
             <TableHead>
               <TableRow>
-                <TableCell>Dessert (100g serving)</TableCell>
+                <TableCell>Food (100g serving)</TableCell>
                 <TableCell align="right">Calories</TableCell>
                 <TableCell align="right">Fat&nbsp;(g)</TableCell>
                 <TableCell align="right">Carbs&nbsp;(g)</TableCell>
